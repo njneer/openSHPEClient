6004c67c4110b2e844b541245f9a30d443229e73
Fausto Carrera <fcarrera@gmail.com>
2011-03-26 15:07:52 -0700

    Header Update

--- openSHPEClient/openSHPEClientAppDelegate.h
+++ openSHPEClient/openSHPEClientAppDelegate.h
@@ -3,7 +3,7 @@
 //  openSHPEClient
 //
 //  Created by Fausto Carrera on 3/26/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 SHPE Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
