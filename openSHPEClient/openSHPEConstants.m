629eac32a1b819db4be14e3181117d5399a5311c
Fausto Carrera <fcarrera@gmail.com>
2011-04-02 14:58:44 -0700

    Added openSHPEConstants class.
    Will hold constants for ease of use purposes and to make API specific changes in a single file.

---
+++ openSHPEClient/openSHPEConstants.m
@@ -0,0 +1,14 @@
+//
+//  openSHPEConstants.m
+//  openSHPEClient
+//
+//  Created by Fausto Carrera on 4/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "openSHPEConstants.h"
+
+
+@implementation openSHPEConstants
+
+@end
